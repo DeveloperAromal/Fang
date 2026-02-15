d10408054a5aaf6ccf27e95dee61969d0774b9d6
DeveloperAromal <developeraromal@gmail.com>
2026-02-15 19:17:15 +0530

    Implement TechFingerprint class with backend and meta detection; enhance WebScraper for improved response handling; update main.py for fingerprinting usage; add __init__.py for module structure.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fang.utils.banner import banner
 from fang.modules.web.basic.subdomain_enumerator import SubdomainEnumerator
+from fang.modules.web.basic.tech_fingerprint import TechFingerprint
 
 banner()
 
 SubdomainEnumerator("https://www.steyp.com").scan()
+
+TechFingerprint("https://www.steyp.com")
+
+fp = TechFingerprint("https://www.steyp.com")
+result = fp.fingerprint()
+print(result)
