c2d85ccef0fde9705525160eabe838bb5b809130
DeveloperAromal <developeraromal@gmail.com>
2026-02-16 06:03:52 +0530

    Add RobotsParser class for parsing robots.txt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fang.utils.banner import banner
 from fang.modules.web.basic.subdomain_enumerator import SubdomainEnumerator
 from fang.modules.web.basic.tech_fingerprint import TechFingerprint
+from fang.modules.web.basic.robots_parser import RobotsParser
 
 banner()
 
@@ -11,3 +12,8 @@ TechFingerprint("https://www.steyp.com")
 fp = TechFingerprint("https://www.steyp.com")
 result = fp.fingerprint()
 print(result)
+
+rp = RobotsParser("https://www.nytimes.com").parse()
+
+print(rp)
+
